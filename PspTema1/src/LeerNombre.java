267f16732deb3d72b7743c06e6bfd7b5cf5c7bc0
andreesramos <andresrn16@gmail.com>
2024-09-23 13:28:52 +0200

    prueba

---
+++ PspTema1/src/LeerNombre.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Arrays;
+
+public class LeerNombre {
+
+    public static void main(String[] args) throws IOException {
+        if(args.length <= 0){
+            System.err.println("Se necesita un programa a ejecutar");
+            System.exit(-1);
+        }else{
+            System.out.println(Arrays.toString(args));
+            System.exit(1);
+        }
+
+        try{
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
